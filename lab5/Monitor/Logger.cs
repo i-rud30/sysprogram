72d0eef3fa6bb26b0c98e9e4ecd3c0b72a2a580c
i-rud30 <i.v.rudj@student.csn.khai.edu>
2023-12-15 21:26:00 +0200

    first commit

---
+++ lab5/Monitor/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+
+namespace Monitor
+{
+    public class Logger
+    {
+        private readonly string filePath;
+
+        public Logger(string filePath)
+        {
+            this.filePath = filePath;
+            ClearLogFile();
+        }
+
+        private void ClearLogFile()
+        {
+            File.WriteAllText(filePath, string.Empty);
+        }
+
+        public void Log(string message)
+        {
+            File.AppendAllText(filePath, message + Environment.NewLine);
+        }
+    }
+}
